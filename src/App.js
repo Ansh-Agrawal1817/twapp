c99873c4afefae42b36e0878cb86efbab9224a98
hitarth <goforhitarth@gmail.com>
2024-04-05 11:05:38 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import "@fontsource/inter"; // Defaults to weight 400
+import "@fontsource/inter/100.css"; // Specify weight
+import "@fontsource/inter/200.css"; // Specify weight
+import "@fontsource/inter/300.css"; // Specify weight
+import "@fontsource/inter/400.css"; // Specify weight
+import "@fontsource/inter/500.css"; // Specify weight
+import "@fontsource/inter/600.css"; // Specify weight
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Hero />
+      <Features />
     </div>
   );
 }
